5835fd678d2a6820ddd6de751aee9d69bfd7ecea
kkulma <kkulma@mango-solutions.com>
2020-02-15 15:04:04 +0000

    separated the reactives

--- server.R
+++ server.R
@@ -12,29 +12,9 @@ server <- server <- function(input, output, session) {
     DT::datatable(output, filter = "top", options = list(scrollX = TRUE))
   })
   
-  # output$filtered_map <- reactive({
-  #   req(input$food)
-  #   subset(map2, food_category == input$food)
-  # })
-  #
-  # output$data_to_map <- reactive({
-  #   req(input$map_var)
-  #
-  #   switch(input$map_var,
-  #          "CO2 emissions" = filtered_map()$co2_emmission,
-  #          "Food Consumption" = filtered_map()$consumption)
-  # })
-  
-  
-  
-  
-  output$map <- renderLeaflet({
+  filtered_map <- reactive({
     req(input$food)
-    req(input$map_var)
-    
     filtered_map <- subset(map2, food_category == input$food)
-    
-    # create over label
     filtered_map$labels <- paste0(
       "<strong> Country: </strong> ",
       filtered_map$NAME,
@@ -48,23 +28,43 @@ server <- server <- function(input, output, session) {
     ) %>%
       lapply(htmltools::HTML)
     
-    
+    filtered_map
+  })
+  
+
+  data_to_map <- reactive({
+    req(input$map_var)
+
+    switch(input$map_var,
+           "CO2 emissions" = filtered_map()$co2_emmission,
+           "Food Consumption" = filtered_map()$consumption)
+  })
+
+  
+  # pal <- reactive({
+  #   colorBin(
+  #   palette = "viridis",
+  #   domain = filtered_map()$co2_emmission,
+  #   bins = 7)
+  # })
+  
+  output$map <- renderLeaflet({
+    req(input$food)
+    req(input$map_var)
     
     pal <- colorBin(
       palette = "viridis",
-      domain = filtered_map$co2_emmission,
-      bins = 7
-    )
+      domain = data_to_map(),
+      bins = 7)
     
-    
-    leaflet(filtered_map) %>%
+    leaflet(filtered_map()) %>%
       addTiles() %>%
       setView(lng = 0,
               lat = 30,
               zoom = 2) %>%
       addProviderTiles(providers$CartoDB.Positron) %>%
       addPolygons(
-        fillColor = ~ pal(filtered_map$co2_emmission),
+        fillColor = ~ pal(data_to_map()),
         color = "white",
         fillOpacity = 0.7,
         label = ~ labels,
@@ -73,7 +73,7 @@ server <- server <- function(input, output, session) {
       ) %>%
       leaflet::addLegend(
         pal = pal,
-        values = ~ filtered_map$co2_emmission,
+        values = ~ data_to_map(),
         opacity = 0.7,
         title = paste0(input$map_var)
       )
